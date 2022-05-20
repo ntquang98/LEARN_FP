bc1667f89014d5eb3f00134b5fd1177e6e26ab5b
Quang Nguyen <quang@codelynx.io>
2022-05-20 16:41:54 +0700

    init ex1

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const { curry, compose, pipe } = require('./helper');
 
-const scream = str => str.toUpperCase();
-const exclaim = str => `${str}!`;
-const repeat = str => `${str} ${str}`;
+const first = (xs) => xs[0];
 
-const withExuberance = compose(repeat, exclaim, scream);
+const formatInitial = (x) => x.toUpperCase() + ".";
 
-const result = withExuberance('hello world');
-console.log(result);
+const map = (fn, arr) => arr.map(fn);
+
+const intercalate = (sep, arr) => arr.join(sep);
+
+const employees = ["Javascript", "Rust", "Golang", "Python"];
+
+const initials = intercalate(" - ", map(formatInitial, map(first, employees)));
+// J. - R. - G. - P.
