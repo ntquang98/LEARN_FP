5044d54956b0236229bb58efbf10818ee837a5d0
Quang Nguyen <quang@codelynx.io>
2022-05-20 16:40:51 +0700

    first

--- index.js
+++ index.js
@@ -8,20 +8,3 @@ const withExuberance = compose(repeat, exclaim, scream);
 
 const result = withExuberance('hello world');
 console.log(result);
-
-// EX1 
-
-const first = (xs) => xs[0];
-
-const formatInitial = (x) => x.toUpperCase() + ".";
-
-const map = (fn, arr) => arr.map(fn);
-
-const intercalate = (sep, arr) => arr.join(sep);
-
-const employees = ["Javascript", "Rust", "Golang", "Python"];
-
-const initials = intercalate(" - ", map(formatInitial, map(first, employees)));
-// J. - R. - G. - P.
-
-console.log(initials);
