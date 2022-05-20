2e4ef6ed1a185b8c3793bfe9a37de06899448c76
Quang Nguyen <quang@codelynx.io>
2022-05-20 19:09:15 +0700

    final ans

--- index.js
+++ index.js
@@ -8,7 +8,20 @@ const map = (fn, arr) => arr.map(fn);
 
 const intercalate = (sep, arr) => arr.join(sep);
 
-const employees = ["Javascript", "Rust", "Golang", "Python"];
+const employees = ["Jame", "Ruby", "Elsa", "Tommy"];
 
 const initials = intercalate(" - ", map(formatInitial, map(first, employees)));
 // J. - R. - G. - P.
+
+const cond = (x) => (y) => x !== y;
+const my_if = cond => x => y => cond(x)(y) ? 't' : 'f';
+
+console.log(my_if(cond)(3)(5));
+
+const getListFirstChars = curry(map)(first);
+
+const getListFormattedDot = curry(map)(formatInitial);
+
+const intercalateWithHyphen = curry(intercalate)(" - ");
+
+compose(console.log, intercalateWithHyphen, getListFormattedDot, getListFirstChars)(employees);
